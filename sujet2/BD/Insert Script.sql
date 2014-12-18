29f02ccb250a82cd2d3f64fd8a540761a6bda774
Jmmartinezro <Jmmartinezro@hotmail.com>
2014-12-18 15:27:18 +0100

    Created Insert Script
    Add the values you want to put in the BD here !

---
+++ sujet2/BD/Insert Script.sql
@@ -0,0 +1,2 @@
+INSERT INTO sujet2.member (memberEmail, memberLogin, memberPassword, memberIsAdmin, memberJoiningDate, memberLastname, memberFirstname, memberBirthday, memberNationality, memberSex, memberProfession, memberIsSuppressed) 
+	VALUES ('Jmmartinezro@gmail.com', 'Jmmr', '1234', true, CURRENT_DATE, 'Martinez', 'Juan', CURRENT_DATE, 'Colombia', 'Male', 'Student', false)
