2690d4d7833f47a2f297195cf752d8bf72ff5656
MonssefLeGris <mfaghloumi@gmail;com>
2015-01-03 12:21:47 +0100

    Fichiers manguants : MemberbacksProjectService MemberbacksProjectFacade form_deleteproject

---
+++ sujet2/src/main/java/com/dac2014equipe3/sujet2/businesslogic/service/MemberbacksProjectService.java
@@ -0,0 +1,59 @@
+package com.dac2014equipe3.sujet2.businesslogic.service;
+
+import com.dac2014equipe3.sujet2.model.dao.DAOFactory;
+import com.dac2014equipe3.sujet2.model.entity.MemberbacksProject;
+import com.dac2014equipe3.sujet2.model.entity.MembercreatesProject;
+import com.dac2014equipe3.sujet2.vo.MemberbacksProjectVo;
+import com.dac2014equipe3.sujet2.vo.MembercreatesProjectVo;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Monssef on 03/01/15.
+ */
+public class MemberbacksProjectService implements IService<MembercreatesProjectVo>{
+
+    private static MemberbacksProjectService instance;
+
+    public static synchronized MemberbacksProjectService getInstance() {
+        if (instance == null) {
+            instance = new MemberbacksProjectService();
+        }
+        return instance;
+    }
+
+    @Override
+    public void persist(MembercreatesProjectVo membercreatesProjectVo, EntityManager em) {
+
+    }
+
+    @Override
+    public MembercreatesProjectVo find(Object id, EntityManager em) {
+        return null;
+    }
+
+    @Override
+    public void update(MembercreatesProjectVo membercreatesProjectVo, EntityManager em) {
+
+    }
+
+    @Override
+    public void delete(Object id, EntityManager em) {
+
+    }
+
+    @Override
+    public List<MembercreatesProjectVo> getList(EntityManager em) {
+        return null;
+    }
+
+    public List<MemberbacksProjectVo> getListForProject(Object idProject, EntityManager em) {
+        List<MemberbacksProjectVo> list = new ArrayList<MemberbacksProjectVo>();
+        for (MemberbacksProject MbP : DAOFactory.getInstance().getMemberbacksProjectDAO().getListForProject(idProject, em)) {
+            list.add((MbP).toVo());
+        }
+        return list;
+    }
+}
